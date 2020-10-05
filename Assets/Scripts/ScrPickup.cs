67a06a50ede28ccd5b6b579e4cfc5dde5db37915
Mathias Miranda <mirandaum@campusemav.com>
2020-10-05 11:05:02 +0200

    05/10
    fins a controlador de player

--- Assets/Scripts/ScrPickup.cs
+++ Assets/Scripts/ScrPickup.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TestScript : MonoBehaviour
+public class ScrPickup : MonoBehaviour
 {
+    [SerializeField]
+    int velGir = 3;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,7 @@ public class TestScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // Time.deltaTime (grados * segundo)
+        transform.Rotate(0, 0, velGir * Time.deltaTime);
     }
 }
---
+++ Assets/Scripts/ScrPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrPlayer : MonoBehaviour
+{
+    [SerializeField]
+    float velocitat=3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x, y;  //Para leer el gamepad
+        x = Input.GetAxis("Horizontal"); //Lectura Horizontal
+        y = Input.GetAxis("Vertical");  //Lectura Vertical
+        //print("Horizontal: " + y + " / Vertical: " + y);
+        transform.Translate(x * Time.deltaTime * velocitat, y * Time.deltaTime * velocitat, 0);
+        
+    }
+}
