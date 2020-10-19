9837e8ba1534bceef9e4546df4ae98daf9eca506
Mathias Miranda <mirandaum@campusemav.com>
2020-10-19 09:59:07 +0200

    Classe 19/10/20
    Fins puntuacio pickups

---
+++ Assets/Scripts/ScrControlGame.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrControlGame : MonoBehaviour
+{
+    public static int punts = 0; //puntuació, llegir les boles que es menja(desapareix) Només hi ha 1 per tot el joc.
+}
--- Assets/Scripts/ScrPlayer.cs
+++ Assets/Scripts/ScrPlayer.cs
@@ -4,22 +4,44 @@ using UnityEngine;
 
 public class ScrPlayer : MonoBehaviour
 {
+
     [SerializeField]
-    float velocitat=3;
+    float fuerza = 10; 
+    float x, y; //variables que guardan la lectura del teclado
+
+    //Para acceder al componente Rigidbody2D(rb de rigidbody):
+    Rigidbody2D rb; //Tipo nombre;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>(); //apunta al component rigidbody del player
     }
 
     // Update is called once per frame
-    void Update()
+    void Update() //cada fotograma actualiza y hace lo que hemos escrito a continuacion, depende de la maquina (void Update)
     {
-        float x, y;  //Para leer el gamepad
+        /*float x, y;  //Para leer el gamepad*/
         x = Input.GetAxis("Horizontal"); //Lectura Horizontal
         y = Input.GetAxis("Vertical");  //Lectura Vertical
         //print("Horizontal: " + y + " / Vertical: " + y);
-        transform.Translate(x * Time.deltaTime * velocitat, y * Time.deltaTime * velocitat, 0);
+        /*transform.Translate(x * Time.deltaTime * velocitat, y * Time.deltaTime * velocitat, 0);*/
         
     }
-}
+    private void FixedUpdate() //cada tiempo, ex: cada decima de segundo hace lo que hemos escrito a continuacion, no depende de la maquina, no hace falta "deltaTime" (void FixedUpdate)
+    {
+        rb.AddForce(new Vector2(x*fuerza, y*fuerza));
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        //if (collision.tag == "pickup") Destroy(collision.gameObject);
+        if (collision.CompareTag("pickup"))
+        {
+            Destroy(collision.gameObject);
+            ScrControlGame.punts += 5;
+            print("Puntuació: " + ScrControlGame.punts);
+        }
+    }
+}   
